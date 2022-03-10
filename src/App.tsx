8766fbf40153f2ee00f7416a987766e3435b0d44
Alexandra <bguevaraale@gmail.com>
2022-03-10 20:18:09 -0300

    feature(table): add logic to get json and apply it to generate rows to table dinamically

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import axios from 'axios';
+import { Container, TablePagination } from '@material-ui/core';
 
 const useStyles = makeStyles({
   table: {
@@ -14,46 +16,63 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 export default function TablePage() {
   const classes = useStyles();
+  const [posts, setPosts] = React.useState<any>([]);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+  
+  React.useEffect(() => {
+    const fetchPosts = async ()  => {
+      const res:any = await axios.get('https://jsonplaceholder.typicode.com/comments');
+      setPosts(res.data)
+    }
+    fetchPosts();
+      
+  }, []);
   return (
-  <TableContainer component={Paper}>
-    <Table className={classes.table} size="small" aria-label="a dense table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <TableRow key={row.name}>
-            <TableCell component="th" scope="row">
-              {row.name}
-            </TableCell>
-            <TableCell align="right">{row.calories}</TableCell>
-            <TableCell align="right">{row.fat}</TableCell>
-            <TableCell align="right">{row.carbs}</TableCell>
-            <TableCell align="right">{row.protein}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+    <Container>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell align="right">E-mail</TableCell>
+              <TableCell align="right">Conteúdo</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {posts.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((post:any) => (
+              <TableRow key={post.name}>
+                <TableCell component="th" scope="post">
+                  {post.name}
+                </TableCell>
+                <TableCell align="right">{post.email}</TableCell>
+                <TableCell align="right">{post.body}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={posts.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+    </Container>
   );
 }
