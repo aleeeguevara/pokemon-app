9bf104de9d341f9b5de81903c7aeafe037626a3d
Alexandra <bguevaraale@gmail.com>
2022-03-12 16:25:30 -0300

    feature(table): add ternary condition to joinItems function

--- src/utils/joinItems.ts
+++ src/utils/joinItems.ts
@@ -1,9 +1,6 @@
-export const joinItems = (items: any, attr: string) => {
-  return items.reduce((text: string, item: any, index: number) => {
-    if (index === 0) {
-      return item[attr].name
-    }
-
-    return `${text}, ${item[attr].name}` 
-  }, '')
-};
+    export const joinItems = (items: any, attr: string) =>
+    items.reduce(
+      (text: string, item: any, index: number) =>
+        index === 0 ? item[attr].name : `${text}, ${item[attr].name}`,
+      ""
+    );
